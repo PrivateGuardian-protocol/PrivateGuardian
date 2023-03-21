94f756dc1c33527b78d6b5fcbb39dd4f102483d2
Mobius <chunloved@gmail.com>
2023-03-21 15:45:53 +0700

    feat: add download trust setup params

--- compileCircuitExample.sh
+++ compileCircuitExample.sh
@@ -8,17 +8,28 @@ for name in "${names[@]}"; do
   cd circuits
   # rebuild the r1cs and zkey and verification key
   circom ./${name}.circom --r1cs --wasm -o ./compile
-  mkdir -p ../hardhat/statics
-  mv ./compile/${name}_js/${name}.wasm ../hardhat/statics
+  mkdir -p ../statics
+  mv ./compile/${name}_js/${name}.wasm ../statics
   rm -r ./compile/${name}_js
-  # TODO: We should download pot file first
-  snarkjs ${algo} setup ./compile/${name}.r1cs ./compile/trust_setup/pot${version}_final.ptau ./compile/${name}_0000.zkey
+
+  # download pot file first
+  if [ -f ./compile/trust_setup/powersOfTau28_hez_final_${version}.ptau ]; then
+    echo "powersOfTau28_hez_final_${version}.ptau already exists. Skipping."
+  else
+    mkdir -p ./compile/trust_setup
+    cd ./compile/trust_setup
+    echo "Downloading powersOfTau28_hez_final_${version}.ptau"
+    wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_${version}.ptau
+    cd ../..
+  fi
+
+  snarkjs ${algo} setup ./compile/${name}.r1cs ./compile/trust_setup/powersOfTau28_hez_final_${version}.ptau ./compile/${name}_0000.zkey
   snarkjs zkey contribute ./compile/${name}_0000.zkey ./compile/${name}.zkey --name="1st Contributor Name" -v -e="random text xxxxxx"
 
-  cp ./compile/${name}.zkey ../hardhat/statics
+  cp ./compile/${name}.zkey ../statics
   mkdir -p ./compile/frontend
-  cp ../hardhat/statics/${name}.zkey ./compile/frontend
-  cp ../hardhat/statics/${name}.wasm ./compile/frontend
+  cp ../statics/${name}.zkey ./compile/frontend
+  cp ../statics/${name}.wasm ./compile/frontend
 
   # export solidity verfier
   snarkjs zkey export solidityverifier ./compile/${name}.zkey ../contracts/libraries/${name}Verifier.sol
